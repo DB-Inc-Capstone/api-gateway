5742acb711e6b72889a1f00a7f7d12b99a0533af
DevTae <dev.taehyeon.kim@gmail.com>
2024-05-06 01:46:48 +0900

    Update codes

--- src/main/java/kr/co/dbinc/BackApigatewayApplication.java
+++ src/main/java/kr/co/dbinc/BackApigatewayApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BackApigatewayApplication {
