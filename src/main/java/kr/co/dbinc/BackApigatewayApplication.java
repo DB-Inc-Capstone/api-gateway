792f24a3279deeb56023b9cbb2da6348ea60dd81
DevTae <dev.taehyeon.kim@gmail.com>
2024-05-06 15:45:13 +0900

    Update application.yml to fix CORS problem

--- src/main/java/kr/co/dbinc/BackApigatewayApplication.java
+++ src/main/java/kr/co/dbinc/BackApigatewayApplication.java
@@ -3,9 +3,7 @@ package kr.co.dbinc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
 @EnableDiscoveryClient
 @SpringBootApplication
 public class BackApigatewayApplication {
